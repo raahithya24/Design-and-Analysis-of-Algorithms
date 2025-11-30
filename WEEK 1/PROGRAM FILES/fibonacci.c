a2adcb90ef1ec508d5d5c3e9ad56761f286ed167
raahithya24 <madduraahithyayadav@gmail.com>
2025-11-30 16:07:56 +0530

    Update fibonacci.c

--- WEEK 1/PROGRAM FILES/fibonacci.c
+++ WEEK 1/PROGRAM FILES/fibonacci.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 int main(){
-int a,b,c,n;
-printf("enter the a number n\n");
-scanf("%d",&n);
-a=0;
-b=1;
-printf("the fibonacci numbers are \n");
-while(a<n){
-printf("%d ",a);
- c=a+b;
- a=b;
- b=c;
- }
- }
-
+  int n,i;
+  int a=0, b=1, c;
+  printf("Enter the number of Fibonacci Numbers: ");
+  scanf("%d" , &n);
+  if(n<=0){
+    printf("Please enter a positive integer");
+    return 0;
+  }
+  printf("Fibonacci Series: ");
+  for (i=1; i<=n;i++){
+    if (i==1){
+      printf("%d ", a);
+      continue;
+    }
+    if (i==2){
+      printf("%d ",b);
+      continue;
+    }
+    c = a+b;
+    printf("%d ", c);
+    a=b;
+    b=c;
+  }
+  printf("\n");
+  return 0;
+}
